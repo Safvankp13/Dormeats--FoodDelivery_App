9b18246d8c7633d2f94417486a11d88e0d145eb5
Safvan K.p <safvankp730@gmail.com>
2025-04-20 21:00:42 +0530

    modified:   Backend/db/db.js

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -3,17 +3,15 @@ const { Pool } = pg;
 import dotenv from "dotenv";
 dotenv.config();
 
-
 const pool = new Pool({
-    user: process.env.DB_USER,  
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-});
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }, 
+  });
 
-pool.connect()
-    .then(() => console.log("✅ Connected to PostgreSQL"))
-    .catch(err => console.error("❌ Database connection error:", err));
-
-export default pool
+  pool.query('SELECT NOW()', (err, res) => {
+    if (err) {
+      console.error('Error executing query:', err.stack);
+    } else {
+      console.log('Connected to the database:', res.rows);
+    }
+  });
